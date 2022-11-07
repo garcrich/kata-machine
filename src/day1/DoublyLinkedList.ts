dd0359580dcb5530c2f16fe7232b479625e117bb
Ricky Garcia <rg3646@outlook.com>
2022-11-06 22:28:30 -0700

    stacks and queues

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -1,27 +1,34 @@
+type Node<T> = {
+    value: T
+    prev?: Node<T>
+}
+
 export default class DoublyLinkedList<T> {
     public length: number;
+    private head?: Node<T>
 
-    
 
     constructor() {
+        this.head = undefined;
+        this.length = 0;
     }
 
     prepend(item: T): void {
 
-}
+    }
     insertAt(item: T, idx: number): void {
 
-}
+    }
     append(item: T): void {
 
-}
+    }
     remove(item: T): T | undefined {
 
-}
+    }
     get(idx: number): T | undefined {
 
-}
+    }
     removeAt(idx: number): T | undefined {
 
-}
+    }
 }
